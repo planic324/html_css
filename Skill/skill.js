9f4b56e1182fdc6301bd89ec9bb461fd9afb1de1
RohEunChan <nong0324@naver.com>
2022-08-19 11:39:12 +0900

    [추가] 아날로그 시계 만들기

--- Skill/skill.js
+++ Skill/skill.js
@@ -13,4 +13,20 @@ window.addEventListener('load', function() {
             xhttp.send();
         }
     });
-});w
+});
+
+var deg = 6;
+var hr = document.querySelector('#hr');
+var mn = document.querySelector('#mn');
+var sc = document.querySelector('#sc');
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = 'rotateZ(${hh+(mm/12)}deg)';
+    mn.style.transform = 'rotatez(${(mm)}deg)';
+    sc.style.transform = 'rotatez(${(ss)}deg)';
+});
