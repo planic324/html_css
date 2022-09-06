056e7ed90dcf846d4ee3abe2376c0083d3b0112d
RohEunChan <nong0324@naver.com>
2022-09-06 17:30:40 +0900

    snb 영역 select 테스트

--- compornent/skill.js
+++ compornent/skill.js
@@ -1,17 +1,16 @@
 window.addEventListener('load', function() {
-    var allElements = document.getElementsByTagName('*');
-    Array.prototype.forEach.call(allElements, function(el) {
-        var includePath = el.dataset.includePath;
-        if (includePath) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    el.outerHTML = this.responseText;
-                }
-            };
-            xhttp.open('GET', includePath, true);
-            xhttp.send();
-        }
-    });
+    // let list = this.document.querySelector('.skill__list');
+    // let listItemNum = document.querySelector('.skill__list--item a').href.search(/skill_/gi);
+    // let htmlLocation = window.location.pathname;
+    // let array = new Array(list);
+    
+    // let htmlLocationFind = htmlLocation.search(/skill_/gi);
+
+    
+    
+    // for( let i = 0; i < )
+    
+    
+
 });
 
